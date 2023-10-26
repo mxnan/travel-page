408a5ee266f0b953cf505fa8e4c7965c9f0be408
mxnan <128995981+mxnan@users.noreply.github.com>
2023-10-27 01:21:44 +0530

    working on hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
-    <main>
-      <h1 className='font-bold text-teal-400 text-2xl'>
-      Hello world
-      </h1>
-    </main>
-    
+      <>
+        <Hero />
+        <Camp />
+        <Guide />
+        <Features />
+        <GetApp />
+      </>  
   )
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image"
+
+
+const Hero = () => {
+  return (
+    <section className="border-2 border-purple-500 max-container 
+    padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 
+    lg:py-20 xl:flex-row">
+      <div className="hero-map" />
+      <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
+         <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left -[-5px] top-[-30px] w-10 lg:w-[50px]"
+         />
+         <h1 className="bold-52 lg:bold-88">
+          Lorem ipsum dolor siter
+         </h1>
+         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+           placeat labore blanditiis cumque doloremque maiores quo, ex 
+           minus molestiae eos? Lorem ipsum dolor sit amet consectetur 
+           adipisi .
+         </p>
+         <div className="my-11 flex flex-wrap">
+           <div className="flex items-center gap-2">
+             {
+              Array(5).fill(1).map((_, index) => (
+                <Image 
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))
+             }
+           </div>
+           <p className="bold-16 lg:bold-20 text-blue-70">
+            198K
+           <span>Excellent Reviews</span> 
+           </p>
+         </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
