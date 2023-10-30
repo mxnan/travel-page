6ff5e41b8260a1a29d46c7b240bf812183e129e1
mxnan <128995981+mxnan@users.noreply.github.com>
2023-10-30 19:11:48 +0530

    hero page done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
+import Button from "./Button"
 
 
 const Hero = () => {
   return (
-    <section className="border-2 border-purple-500 max-container 
-    padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 
-    lg:py-20 xl:flex-row">
+    <section className="max-container padding-container flex flex-col 
+    gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
       <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
          <Image
@@ -24,8 +24,8 @@ const Hero = () => {
            minus molestiae eos? Lorem ipsum dolor sit amet consectetur 
            adipisi .
          </p>
-         <div className="my-11 flex flex-wrap">
-           <div className="flex items-center gap-2">
+         <div className="my-11 flex flex-wrap gap-3">
+           <div className="flex items-center gap-1">
              {
               Array(5).fill(1).map((_, index) => (
                 <Image 
@@ -39,11 +39,57 @@ const Hero = () => {
              }
            </div>
            <p className="bold-16 lg:bold-20 text-blue-70">
-            198K
-           <span>Excellent Reviews</span> 
+              198K
+               <span className="regular-16 lg:regular-20 ml-2">
+                 Excellent Reviews
+               </span> 
            </p>
          </div>
+
+         <div className="flex flex-col w-full gap-3 sm:flex-row">
+              <Button
+                type="button"
+                title="Download App"
+                variant="btn_green"
+              />
+               <Button
+                type="button"
+                title="How we work?"
+                icon="/play.svg"
+                variant="btn_white_text"
+              />
+         </div>    
       </div>
+      <div className="relative flex flex-1 items-start">
+         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl
+           bg-green-90 px-7 py-8">
+
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image
+                src="/close.svg"
+                alt="close"
+                width={24}
+                height={24}
+              />
+            </div>
+            <p className="bold-20 text-white">Aguas Calientes</p>
+          </div>
+            
+            <div className="flexBetween">
+             <div className="flex flex-col">
+                 <p className="regular-16 block text-gray-20">Distance</p>
+                 <p className="bold-20 text-white">173.28 km</p>
+             </div>
+             <div className="flex flex-col">
+                 <p className="regular-16 block text-gray-20">Elevation</p>
+                 <p className="bold-20 text-white">1.98 km</p>
+             </div>
+            </div>
+           </div>
+         </div>
+      
     </section>
   )
 }
